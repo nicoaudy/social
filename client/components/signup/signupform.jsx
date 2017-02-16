45a2db85ccff721b091a458984244337835fc133
Nico Kantor <nicoaudy1994@gmail.com>
2017-02-16 11:27:25 +0700

    authentication methods

--- client/components/signup/signupform.jsx
+++ client/components/signup/signupform.jsx
@@ -1,4 +1,46 @@
 Signupform = React.createClass({
+	mixins: [ReactMeteorData],
+	getMeteorData() {
+		const data = {};
+		data.currentUser = Meteor.user();
+		return data;
+	},
+	getInitialState() {
+		return {
+			message: '',
+			messageClass: 'hidden'
+		}
+	},
+	displayError(message) {
+ 		this.setState({message: message, messageClass: 'alert alert-danger registerError'});
+	},
+	handleSubmit(e) {
+		e.preventDefault();
+		this.setState({message: '', messageClass: 'hidden'});
+		var that 		= this;
+		var first_name 	= ReactDOM.findDOMNode(this.refs.first_name).value.trim();
+		var last_name 	= ReactDOM.findDOMNode(this.refs.last_name).value.trim();
+		var email 		= ReactDOM.findDOMNode(this.refs.email).value.trim();
+		var password 	= ReactDOM.findDOMNode(this.refs.password).value.trim();
+        var user = {
+            email: email,
+            password: password,
+            profile: {
+                fullname: (first_name + last_name).toLowerCase(),
+                firstname: first_name,
+                lastname: last_name,
+                avatar: 'http://placehold.it/150x150',
+                friends: []
+            }
+        };
+		Accounts.createUser(user, function(e){
+		    if(e){
+		        that.displayError(e.reason);
+		    } else {
+		        FlowRouter.go('/dashboard');
+		    }
+		})﻿
+	},
 	render() {
 		return (
 			<div className="row">
@@ -8,25 +50,31 @@ Signupform = React.createClass({
 						Its free and always will be
 					</p>
 				</div>
-				<form>
-					<div className="col-md-9">
-						<div className="row">
-							<div className="col-md-6 form-group">
-							<input name="first_name" placeholder="First Name" type="text" ref="first_name" className="form-control" />
-							</div>							
-							<div className="col-md-6 form-group">
-								<input name="last_name" placeholder="Last Name" type="text" ref="last_name" className="form-control" />
-							</div>	
-						</div>										
-						<div className="form-group">
-							<input name="email" placeholder="Email Or Mobile Number" type="text" ref="email" className="form-control" />
-						</div>							
-						<div className="form-group">
-							<input name="password" placeholder="Password" type="password" ref="password" className="form-control" />
-						</div>			
-						<button type="submit" className="btn btn-md btn-success">Signup</button>		
-					</div>
-				</form>
+				
+                <form onSubmit={this.handleSubmit}>
+                    <div className="col-sm-9">
+                        <div className="row">
+                            <div className="col-sm-6 form-group">
+                                <input ref="first_name" type="text" placeholder="First name"
+                                       className="form-control"/>
+                            </div>
+                            <div className="col-sm-6 form-group">
+                                <input ref="last_name" type="text" placeholder="Last name"
+                                       className="form-control"/>
+                            </div>
+                        </div>
+                        <div className="form-group">
+                            <input type="text" placeholder="Email or mobile number" ref="email"
+                                   className="form-control"/>
+                        </div>
+                        <div className="form-group">
+                            <input type="password" placeholder="Password" ref="password"
+                                   className="form-control"/>
+                        </div>
+                        <button type="submit" className="btn btn-md btn-success">Sign Up</button>
+                        <span className={this.state.messageClass}>{this.state.message}</span>
+                    </div>
+                </form>
 			</div>
 		);
 	}
