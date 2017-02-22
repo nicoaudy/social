a83a043f2b4055b2341fa78cc2d4a1e8848db0e9
Nico Kantor <nicoaudy1994@gmail.com>
2017-02-22 11:42:52 +0700

    changeAvatar in profile component

--- client/components/profile/profile.jsx
+++ client/components/profile/profile.jsx
@@ -1,13 +1,43 @@
 Profile = React.createClass({
+    mixins: [ReactMeteorData],
+    getMeteorData() {
+        let data = {};
+        data.currentUser = Meteor.user();
+        return data;
+    },
+    getInitialState(){
+        return {
+            klass:'img-circle img-responsive custom-input-file',
+            editmode:false,
+            email:this.data && this.data.currentUser && this.data.currentUser.emails ? this.data.currentUser.emails[0].address:'you@yourdomain.com'
+        }
+    },
+    componentDidMount(){
+        this.setState({email:this.data.currentUser ? Meteor.user().emails[0].address:''});
+    },
+    uploadFile(e){
+        e.preventDefault();
+        var that = this;
+        var image;
+        FS.Utility.eachFile(e, function (file) {
+            Images.insert(file, function (err, fileObj) {
+                Meteor.call('changeAvatar', Meteor.user(), fileObj._id);
+                setTimeout(function(){
+                    that.setState({klass:'img-circle img-responsive custom-input-file updated'});
+                },100)
+            });
+        });
+    },
+
     render() {
         return (
             <div className="row">
                 <div className="col-md-2 hidden-xs">
-                    Avatar
+                    <Avatar user={this.data.currentUser ? this.data.currentUser._id : ''} klass={this.state.klass} />
                     <div>
                         <label>
                             <div className="inputWrapper">
-                                <input id="avatar" name="avatar" type="file" className="fileInput change-avatar" />
+                                <input onChange={this.uploadFile} id="avatar" name="avatar" type="file" className="fileInput change-avatar" />
                             </div>
                         </label>
                     </div>
@@ -25,4 +55,5 @@ Profile = React.createClass({
             </div>
         )
     }
+
 });
--- server/server.js
+++ server/server.js
@@ -3,12 +3,21 @@ Meteor.methods({
 		var post = {
 			user: Meteor.user(),
 			createdAt: new Date(),
-			image: imageid, 
+			image: imageid,
 			imageurl: imageurl,
 			message: message,
 			likes: [],
 			comments: []
 		};
 		Posts.insert(post);
+	},
+
+	'changeAvatar': function(user,fileid){
+			var file = 'http://placehold.it/150x150';
+			if (fileid) {
+					file = Images.findOne({_id: fileid});
+			}
+			var data = file._id;
+			Meteor.users.update(Meteor.userId(), {$set: {'profile.avatar': data}})
 	}
 });
